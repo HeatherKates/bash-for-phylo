a8f2a21488c672a066b2841a86ed63a174fef9ca
Heather Kates <hkates@ufl.edu>
2020-02-20 14:51:36 -0500

    Update gettips.bash
    Added code to extract subset of tips from alignment based on input tree.

--- gettips.bash
+++ gettips.bash
@@ -1,1 +1,4 @@
+#Print out the tips from a tree
 sed 's/(/\n(/g' Input.tree | sed 's/,/\n/g' | cut -d ":" -f 1 |sed -E -e 's/\(|\)//g'|sed '/^$/d'> Input.tree.tips
+#Extract the matching sequences from an alignment
+while read file; do grep -A1 $file Input_alignment.fasta >> Alignment_subtree.fasta; done < Input.tree.tips
