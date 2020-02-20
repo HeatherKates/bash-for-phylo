454a623d3a103e5d79f86caf3fb2c789386d04d5
Heather Kates <hkates@ufl.edu>
2020-02-20 12:46:09 -0500

    Update gettips.bash
    Escape the parentheses

--- gettips.bash
+++ gettips.bash
@@ -1,1 +1,1 @@
-sed 's/(/\n(/g' Input.tree | sed 's/,/\n/g' | cut -d ":" -f 1 |sed -E -e 's/(|)//g'|sed '/^$/d'> Input.tree.tips
+sed 's/(/\n(/g' Input.tree | sed 's/,/\n/g' | cut -d ":" -f 1 |sed -E -e 's/\(|\)//g'|sed '/^$/d'> Input.tree.tips
